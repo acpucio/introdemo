79392bb908c5a2e2306f1a80ca22e14ad86d7a91
Albert Pucio <acpucio@gmail.com>
2025-06-24 15:21:23 -0500

    asynchronous updated example

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,23 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    console.log('left before', left)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    console.log('left after', left)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    console.log('right before')
+    const updatedRight = right + 1
+    setRight(updatedRight + 1)
+    setTotal(left + updatedRight)
   }
 
   return (
@@ -26,6 +34,7 @@ const App = () => {
       </button>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
