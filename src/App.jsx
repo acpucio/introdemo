2ba8b51aa2d4da82052dbd1653f62c0a11395258
Albert Pucio <acpucio@gmail.com>
2025-06-24 19:46:46 -0500

    passing event handlers to childe components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+)
+
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const setToValue = (newValue) => () =>{
+  const setToValue = (newValue) =>{
     console.log('value now', newValue)
     setValue(newValue)
   }
@@ -11,9 +17,9 @@ const App = () => {
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>zero</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
