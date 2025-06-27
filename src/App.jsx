89ec73b252b0a5174463ae91f6b9bd732d3c11fa
Albert Pucio <acpucio@gmail.com>
2025-06-27 01:38:06 -0500

    Number added

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
-      id: 1
+      number: '867-5309',
+      id: "1"
     }
   ])
 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     
     if(persons.some(person => person.name === newName)){
@@ -19,19 +21,26 @@ const App = () => {
       return 
     }
 
-    const nameObject = {
+    const personObject = {
       name: newName,
+      number: newNumber,
       id: String(persons.length + 1)
     }
 
     console.log('name added', event.target)
-    setPersons(persons.concat(nameObject))
+    setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
 
@@ -41,12 +50,17 @@ const App = () => {
       <h2>
         Phonebook
       </h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name:
           <input
             value={newName}
             onChange={handleNameChange}
+          />
+          number:
+          <input
+            value= {newNumber}
+            onChange= {handleNumberChange}
           />
         </div>
         <div>
@@ -56,7 +70,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Name name={person.name} key={person.id} />)}
+          <Name key={person.id} name={person.name} number={person.number} />)}
       </ul>
     </div>
   )
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,5 +1,6 @@
-const Name = ({ name }) => {
-  return <li>{name}</li>
+const Name = ({ name, number }) => {
+  console.log("The name is", name)
+  return <li>{name}   {number}</li>
 }
 
 export default Name
