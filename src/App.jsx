060e76d8356b6c8c5dee363e45fcdc02532ee364
Albert Pucio <acpucio@gmail.com>
2025-06-27 01:13:42 -0500

    Added Names and filter for existing name

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,66 @@
-import Course from './components/Course'
+import { useState } from 'react'
+import Name from './components/Name'
 
-const Courses = ({courses}) => {
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      id: 1
+    }
+  ])
+
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+    if(persons.some(person => person.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+      return 
+    }
+
+    const nameObject = {
+      name: newName,
+      id: String(persons.length + 1)
+    }
+
+    console.log('name added', event.target)
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
 
   return (
     <div>
-      { courses.map(course => 
-          <Course course = {course} />
-      )}
+      <h2>
+        Phonebook
+      </h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Name name={person.name} key={person.id} />)}
+      </ul>
     </div>
   )
 }
 
-const App = () => {
-  const courses = [
-    {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  return <Courses courses={courses} />
-  
-}
-
 export default App
+
